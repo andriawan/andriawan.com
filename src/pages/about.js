dc5f2ebd26b9868ca960019cb8b23b229c2903fd
Muhammad Irwan Andriawan <irwan@andriawan.com>
2020-03-08 21:27:51 +0700

    update description alignment. resize photo.

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,20 +14,32 @@ function AboutPage() {
 
       <section className="flex flex-col items-center md:flex-row">
         <div className="md:w-2/3 md:mr-8">
-          <blockquote className="pl-4 font-serif leading-loose text-justify">
+          <p className="pl-4 font-serif">
             Nama saya adalah Muhammad Irwan Andriawan. Seorang Pengembang
-            software sejak 2015. Lahir di Jember, Jawa Timur. Saat ini sedang
-            berkarir di Jakarta sebagai seorang Web Developer Sebuah Perusahaan
-            Software House. Fokus pengembangan saya mencakup Mobile Programming
-            dan Web Programming. Sangat suka memperbarui skill dengan
-            memepelajari hal-hal baru dan kekinian. Aktif Ngeblog dan share
-            kegiatan programming. Sangat terbuka untuk sharing dan kerja sama.
-            Silahkan liat-liat portfolio saya.
-          </blockquote>
+            software sejak 2015.
+          </p>
+          <br></br>
+          <p className="pl-4 font-serif">
+            Lahir di Jember, Jawa Timur. Saat ini sedang berkarir di Jakarta
+            sebagai seorang Web Developer Sebuah Perusahaan Software House.
+            Fokus pengembangan saya mencakup Mobile Programming dan Web
+            Programming.
+          </p>
+          <br></br>
+          <p className="pl-4 font-serif">
+            Sangat suka memperbarui skill dengan memepelajari hal-hal baru dan
+            kekinian. Aktif Ngeblog dan share kegiatan programming. Sangat
+            terbuka untuk sharing dan kerja sama. Silahkan liat-liat portfolio
+            saya.
+          </p>
         </div>
 
         <figure className="w-2/3 md:w-1/3">
-          <img alt="A dog relaxing" src={andriawan} />
+          <img
+            className="text-center m-auto w-48"
+            alt="A dog relaxing"
+            src={andriawan}
+          />
         </figure>
       </section>
     </Layout>
