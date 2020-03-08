0bc619e8a75a3ba2b5ea71527f108ac6f7d21703
Muhammad Irwan Andriawan <irwan@andriawan.com>
2020-03-08 21:28:12 +0700

    format code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,20 @@
 import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import Typed from 'react-typed';
-import Imgx from '../images/bg-masthead.jpg'
+import Typed from "react-typed";
+import Imgx from "../images/bg-masthead.jpg";
 
 function IndexPage() {
   const displayText = [
-    'Hello World :D ^1000',
-    'My Name is Andriawan ^1000',
-    'Welcome to my digital home ^1000']
-  
-    const gradient = {
-      background : 'linear-gradient(to right , #18adfd7d, #1fec93d1)',
-      zIndex: -1
-    }
+    "Hello World :D ^1000",
+    "My Name is Andriawan ^1000",
+    "Welcome to my digital home ^1000"
+  ];
 
-  
+  const gradient = {
+    background: "linear-gradient(to right , #18adfd7d, #1fec93d1)",
+    zIndex: -1
+  };
 
   return (
     <Layout>
@@ -25,18 +24,28 @@ function IndexPage() {
       />
 
       <section className="text-center">
-
-        <img className="object-cover fixed inset-0 w-full h-full" style={{zIndex : -2}} src={Imgx}></img>
-        <div className="bg-dark-gray fixed inset-0 w-full h-full" style={gradient}></div>
+        <img
+          className="object-cover fixed inset-0 w-full h-full"
+          style={{ zIndex: -2 }}
+          src={Imgx}
+        ></img>
+        <div
+          className="bg-dark-gray fixed inset-0 w-full h-full"
+          style={gradient}
+        ></div>
 
         <h1 className="text-4xl font-bold text-white inline-block my-8 p-3">
-          <Typed strings={displayText} typeSpeed={100} fadeOut cursorChar={'_'} loop/>
+          <Typed
+            strings={displayText}
+            typeSpeed={100}
+            fadeOut
+            cursorChar={"_"}
+            loop
+          />
         </h1>
       </section>
     </Layout>
   );
 }
-
-
 
 export default IndexPage;
