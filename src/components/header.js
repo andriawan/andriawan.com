8649b725721903b24b7f8eeefb9624e5d84cc634
Muhammad Irwan Andriawan <irwan@andriawan.com>
2020-03-14 22:51:08 +0700

    fix localstorage checking in header

--- src/components/header.js
+++ src/components/header.js
@@ -18,6 +18,8 @@ function Header({ gradient }) {
   `);
 
   useEffect(() => {
+    if(localStorage.dark === undefined) localStorage.dark = false;
+    
     if (isDark == null) {
       toggleDark(JSON.parse(localStorage.dark));
     }
